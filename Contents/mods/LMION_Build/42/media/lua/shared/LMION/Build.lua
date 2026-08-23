a3e1ac6ecb07f5a495c58da908a69e990d48fb6d
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-23 02:21:40 +0200

    build: scope door normalization to LMION recipes

--- Contents/mods/LMION_Build/42/media/lua/shared/LMION/Build.lua
+++ Contents/mods/LMION_Build/42/media/lua/shared/LMION/Build.lua
@@ -7,10 +7,11 @@ local Build = LMION.Build
 Build.ID = "LMION_Build"
 Build.VERSION = "0.0.3-dev"
 
-local function isLmionDoorEntity(gameScript)
-    return gameScript ~= nil
-        and gameScript.getModID ~= nil
-        and gameScript:getModID() == "LMION_Core"
+local function isLmionBuild(self)
+    return self ~= nil
+        and self.craftRecipe ~= nil
+        and self.craftRecipe.getModID ~= nil
+        and self.craftRecipe:getModID() == "LMION_Build"
 end
 
 local function normalizeBuiltDoor(square, gameScript)
@@ -43,7 +44,7 @@ ISBuildIsoEntity.setInfo = function(self, square, north, sprite, openSprite)
     local spriteScript = self.objectInfo and self.objectInfo:getScript() or nil
     local gameScript = spriteScript and spriteScript:getParent() or nil
 
-    if isLmionDoorEntity(gameScript) then
+    if isLmionBuild(self) then
         normalizeBuiltDoor(square, gameScript)
     end
 
