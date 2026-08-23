227f1101ef0d73020fc94012fcdac3ba497f38ee
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-23 03:47:25 +0200

    fix(build): keep shared module load-safe

--- Contents/mods/LMION_Build/42/media/lua/shared/LMION/Build.lua
+++ Contents/mods/LMION_Build/42/media/lua/shared/LMION/Build.lua
@@ -1,5 +1,4 @@
 require "LMION/Core"
-require "BuildingObjects/ISBuildIsoEntity"
 
 LMION.Build = LMION.Build or {}
 local Build = LMION.Build
@@ -7,49 +6,7 @@ local Build = LMION.Build
 Build.ID = "LMION_Build"
 Build.VERSION = "0.0.3-dev"
 
-local function isLmionBuild(self)
-    return self ~= nil
-        and self.craftRecipe ~= nil
-        and self.craftRecipe.getModID ~= nil
-        and self.craftRecipe:getModID() == "LMION_Build"
-end
-
-local function normalizeBuiltDoor(square, gameScript)
-    if square == nil or gameScript == nil then
-        return
-    end
-
-    local objects = square:getSpecialObjects()
-    for i = objects:size() - 1, 0, -1 do
-        local object = objects:get(i)
-        if instanceof(object, "IsoThumpable")
-            and object:isDoor()
-            and object.getEntityScript ~= nil
-            and object:getEntityScript() == gameScript then
-            local result = LMION.Doors.onCreateDoor({ thumpable = object })
-            if result ~= nil and result.object ~= nil then
-                result.object:transmitCompleteItemToClients()
-            end
-            return
-        end
-    end
-end
-
-if Build._originalSetInfo == nil then
-    Build._originalSetInfo = ISBuildIsoEntity.setInfo
-end
-
-ISBuildIsoEntity.setInfo = function(self, square, north, sprite, openSprite)
-    local result = Build._originalSetInfo(self, square, north, sprite, openSprite)
-    local spriteScript = self.objectInfo and self.objectInfo:getScript() or nil
-    local gameScript = spriteScript and spriteScript:getParent() or nil
-
-    if isLmionBuild(self) then
-        normalizeBuiltDoor(square, gameScript)
-    end
-
-    return result
-end
-
 LMION.registerModule(Build.ID, Build)
 LMION.log("Build", "loaded " .. Build.VERSION)
+
+return Build
