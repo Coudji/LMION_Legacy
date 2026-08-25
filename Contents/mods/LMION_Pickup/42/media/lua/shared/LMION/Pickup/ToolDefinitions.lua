469ccb631ed27bc02e18ee6398f1727a24b90845
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-25 06:32:29 +0200

    Add metal gate moveable tool definitions

---
+++ Contents/mods/LMION_Pickup/42/media/lua/shared/LMION/Pickup/ToolDefinitions.lua
@@ -0,0 +1,40 @@
+require "Moveables/ISMoveableDefinitions"
+
+LMION.Pickup = LMION.Pickup or {}
+local Pickup = LMION.Pickup
+
+local function registerToolDefinitions()
+    local definitions = ISMoveableDefinitions:getInstance()
+
+    definitions.removeToolDefinition("LMIONMetalCrowbar")
+    definitions.removeToolDefinition("LMIONMetalHammer")
+
+    definitions.addToolDefinition(
+        "LMIONMetalCrowbar",
+        {"Tag.Crowbar", "Crowbar"},
+        Perks.MetalWelding,
+        150,
+        "Hammering",
+        true
+    )
+
+    definitions.addToolDefinition(
+        "LMIONMetalHammer",
+        {"Base.Hammer"},
+        Perks.MetalWelding,
+        75,
+        "Hammering",
+        true
+    )
+end
+
+registerToolDefinitions()
+
+if Pickup._toolDefinitionsBootHandler ~= nil then
+    Events.OnGameBoot.Remove(Pickup._toolDefinitionsBootHandler)
+end
+
+Pickup._toolDefinitionsBootHandler = registerToolDefinitions
+Events.OnGameBoot.Add(Pickup._toolDefinitionsBootHandler)
+
+return Pickup
