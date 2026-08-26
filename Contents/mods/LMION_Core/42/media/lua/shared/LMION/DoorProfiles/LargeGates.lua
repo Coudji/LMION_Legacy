99f279bde1bf64588a7898f6a527ce46ed917dca
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-26 14:02:02 +0200

    Refactor: split large gate profiles

---
+++ Contents/mods/LMION_Core/42/media/lua/shared/LMION/DoorProfiles/LargeGates.lua
@@ -0,0 +1,8 @@
+return function(add)
+    add("DoubleDoor", "Large Wooden Gate", "wood", 650, "Woodwork", 400, 300, "Wood", "Nails", nil, "Wood_Solid", "WoodGate", "ZombieThumpWood", "none")
+    add("LargeHardenedWoodenGate", "Large Hardened Wooden Gate", "wood", 750, "Woodwork", 500, 350, "Wood", "Nails", "Screws", "Wood_Solid", "WoodGate", "ZombieThumpWood", "none")
+    add("LargeFarmGate", "Large Farm Gate", "metal", 500, "MetalWelding", 300, 200, "MetalPipe", nil, nil, "Metal_Light", "FarmGate", "ZombieThumpMetalPoleGate", "none")
+    add("DoubleWireGate", "Large Chain-Link Gate", "metal", 850, "MetalWelding", 400, 275, "MetalPipe", "MetalWire", nil, "Metal_Light", "MetalGate", "ZombieThumpChainlinkFence", "none")
+    add("DoubleFenceGate", "Large Scrap Metal Gate", "metal", 850, "MetalWelding", 400, 275, "MetalPipe", "MetalScrap", nil, "Metal_Light", "MetalGate", "ZombieThumpChainlinkFence", "none")
+    add("LargeWroughtIronGate", "Large Wrought Iron Gate", "metal", 1200, "MetalWelding", 500, 375, "MetalBars", "MetalPipe", nil, "Metal_Solid", "MetalPoleGateDouble", "ZombieThumpMetalPoleGate", "none")
+end
