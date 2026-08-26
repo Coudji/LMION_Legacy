2f325e3eaee41b01a723bab9f1ead028bfa809fb
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-26 14:02:45 +0200

    Refactor: split sliding door profiles

---
+++ Contents/mods/LMION_Core/42/media/lua/shared/LMION/DoorProfiles/SlidingDoors.lua
@@ -0,0 +1,4 @@
+return function(add)
+    add("BrownSlidingGlassDoor", "Brown Sliding Glass Door", "glass", 250, "MetalWelding", 250, 0, "MetalPlates", "MetalBars", "Glass", "Glass_Solid", "SlidingGlassDoor", "ZombieThumpWindow", "none")
+    add("WhiteSlidingGlassDoor", "White Sliding Glass Door", "glass", 250, "MetalWelding", 250, 0, "MetalPlates", "MetalBars", "Glass", "Glass_Solid", "SlidingGlassDoor", "ZombieThumpWindow", "none")
+end
