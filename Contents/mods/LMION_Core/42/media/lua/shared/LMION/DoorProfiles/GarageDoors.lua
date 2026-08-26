fe515666a29714f8f24258218aab5df1f4110e1b
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-26 14:01:02 +0200

    Refactor: split garage door profiles

---
+++ Contents/mods/LMION_Core/42/media/lua/shared/LMION/DoorProfiles/GarageDoors.lua
@@ -0,0 +1,9 @@
+return function(add)
+    add("IndustrialGarageDoor", "Industrial Garage Door", "metal", 1200, "MetalWelding", 600, 400, "MetalPlates", "MetalBars", nil, "Metal_Light", "GarageDoor", "ZombieThumpGarageDoor", "none")
+    add("GreenGarageDoor", "Green Garage Door", "metal", 1200, "MetalWelding", 600, 400, "MetalPlates", "MetalBars", nil, "Metal_Light", "GarageDoor", "ZombieThumpGarageDoor", "none")
+    add("WhiteGarageDoor", "White Garage Door", "metal", 1200, "MetalWelding", 600, 400, "MetalPlates", "MetalBars", nil, "Metal_Light", "GarageDoor", "ZombieThumpGarageDoor", "none")
+    add("GreyGarageDoor", "Grey Garage Door", "metal", 1200, "MetalWelding", 600, 400, "MetalPlates", "MetalBars", nil, "Metal_Light", "GarageDoor", "ZombieThumpGarageDoor", "none")
+    add("RollingGarageDoor", "Rolling Garage Door", "metal", 1200, "MetalWelding", 600, 400, "MetalPlates", "MetalBars", nil, "Metal_Light", "GarageDoor", "ZombieThumpGarageDoor", "none")
+    add("RedWindowGarageDoor", "Red Window Garage Door", "metal_glazed", 1000, "MetalWelding", 500, 350, "MetalPlates", "MetalBars", nil, "Metal_Light", "GarageDoor", "ZombieThumpGarageDoor", "none")
+    add("RollingWindowGarageDoor", "Rolling Window Garage Door", "metal_glazed", 1000, "MetalWelding", 500, 350, "MetalPlates", "MetalBars", nil, "Metal_Light", "GarageDoor", "ZombieThumpGarageDoor", "none")
+end
