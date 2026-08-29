e1a2bd6195aa21211aa905572b1f85f91d8d7e36
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-29 11:55:22 +0200

    Load shared variable garage cursor

--- Contents/mods/LMION_Build/42/media/lua/shared/LMION/Build.lua
+++ Contents/mods/LMION_Build/42/media/lua/shared/LMION/Build.lua
@@ -9,6 +9,7 @@ Build.VERSION = "0.0.6-dev"
 require "LMION/Build/LargeGateProfiles"
 require "LMION/Build/VanillaLargeGateLeafConstruction"
 require "LMION/Build/GarageConstruction"
+require "LMION/Build/GarageBuildCursor"
 
 LMION.registerModule(Build.ID, Build)
 LMION.log("Build", "loaded " .. Build.VERSION)
