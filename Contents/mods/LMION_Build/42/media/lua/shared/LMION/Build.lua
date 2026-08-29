e8982421dde1e7c2671f3f84bfef04cd1240de3b
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-29 11:45:42 +0200

    Load variable garage build model

--- Contents/mods/LMION_Build/42/media/lua/shared/LMION/Build.lua
+++ Contents/mods/LMION_Build/42/media/lua/shared/LMION/Build.lua
@@ -8,6 +8,7 @@ Build.VERSION = "0.0.6-dev"
 
 require "LMION/Build/LargeGateProfiles"
 require "LMION/Build/VanillaLargeGateLeafConstruction"
+require "LMION/Build/GarageConstruction"
 
 LMION.registerModule(Build.ID, Build)
 LMION.log("Build", "loaded " .. Build.VERSION)
