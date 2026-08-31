b4e9f1ca4de55ce0aaea7d48dc19ed8aca16f7f2
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-31 15:57:57 +0200

    Enable Large Gate replacement adapter

--- Workshop/Contents/mods/LMION_Pickup/42/media/lua/shared/LMION_Pickup.lua
+++ Workshop/Contents/mods/LMION_Pickup/42/media/lua/shared/LMION_Pickup.lua
@@ -1,5 +1,7 @@
 local MoveableAdapter = require "LMION/Pickup/MoveableAdapter"
 local LargeGatePickup = require "LMION/Pickup/LargeGatePickup"
+local LargeGatePlacement = require "LMION/Pickup/LargeGatePlacement"
 
 MoveableAdapter.install()
 LargeGatePickup.install()
+LargeGatePlacement.install()
