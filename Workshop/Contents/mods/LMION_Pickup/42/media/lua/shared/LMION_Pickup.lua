ff4985bd77a6282175d0d01a1a095e65fc831939
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-31 18:12:04 +0200

    Install parcel presentation after Large Gate pickup

--- Workshop/Contents/mods/LMION_Pickup/42/media/lua/shared/LMION_Pickup.lua
+++ Workshop/Contents/mods/LMION_Pickup/42/media/lua/shared/LMION_Pickup.lua
@@ -1,7 +1,9 @@
 local MoveableAdapter = require "LMION/Pickup/MoveableAdapter"
 local LargeGatePickup = require "LMION/Pickup/LargeGatePickup"
+local ParcelPresentation = require "LMION/Pickup/ParcelPresentation"
 local LargeGatePlacement = require "LMION/Pickup/LargeGatePlacement"
 
 MoveableAdapter.install()
 LargeGatePickup.install()
+ParcelPresentation.install()
 LargeGatePlacement.install()
