1e37ccdcf646e1d6c6e5464518ac52f57b176328
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-31 15:12:25 +0200

    Install large-gate Pickup after door adapter

--- Workshop/Contents/mods/LMION_Pickup/42/media/lua/shared/LMION_Pickup.lua
+++ Workshop/Contents/mods/LMION_Pickup/42/media/lua/shared/LMION_Pickup.lua
@@ -1,3 +1,5 @@
 local MoveableAdapter = require "LMION/Pickup/MoveableAdapter"
+local LargeGateAdapter = require "LMION/Pickup/LargeGateAdapter"
 
 MoveableAdapter.install()
+LargeGateAdapter.install()
