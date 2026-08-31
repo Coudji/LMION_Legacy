024ee19fc53f9e473ec0ead4963e392481b4c8dc
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-31 12:16:44 +0200

    Paired pickup: add BrownChurch geometry

--- Workshop/Contents/mods/LMION_Core/42/media/lua/shared/LMION/Catalog/Doors/Paired/Wooden/BrownChurchDoubleDoor.lua
+++ Workshop/Contents/mods/LMION_Core/42/media/lua/shared/LMION/Catalog/Doors/Paired/Wooden/BrownChurchDoubleDoor.lua
@@ -7,4 +7,27 @@ return {
         left = "Base.BrownChurchDoubleDoorLeft",
         right = "Base.BrownChurchDoubleDoorRight",
     },
+
+    geometry = {
+        N = {
+            left = {
+                closed = "location_community_church_small_01_65",
+                open = "location_community_church_small_01_67",
+            },
+            right = {
+                closed = "location_community_church_small_01_69",
+                open = "location_community_church_small_01_71",
+            },
+        },
+        W = {
+            left = {
+                closed = "location_community_church_small_01_68",
+                open = "location_community_church_small_01_70",
+            },
+            right = {
+                closed = "location_community_church_small_01_64",
+                open = "location_community_church_small_01_66",
+            },
+        },
+    },
 }
