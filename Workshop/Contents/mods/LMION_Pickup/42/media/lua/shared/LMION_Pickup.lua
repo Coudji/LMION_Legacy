6f5c45a788ab35e039f0aa7cd978fe5089acc31e
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-31 15:27:25 +0200

    Reset Large Gate Pickup runtime before rewrite

--- Workshop/Contents/mods/LMION_Pickup/42/media/lua/shared/LMION_Pickup.lua
+++ Workshop/Contents/mods/LMION_Pickup/42/media/lua/shared/LMION_Pickup.lua
@@ -1,5 +1,3 @@
 local MoveableAdapter = require "LMION/Pickup/MoveableAdapter"
-local LargeGateAdapter = require "LMION/Pickup/LargeGateAdapter"
 
 MoveableAdapter.install()
-LargeGateAdapter.install()
