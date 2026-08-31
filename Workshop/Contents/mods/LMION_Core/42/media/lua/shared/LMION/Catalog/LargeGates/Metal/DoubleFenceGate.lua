c0be07d160a5e4c08f566aad23d9fb5bd8f3f28f
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-31 18:57:15 +0200

    Remove duplicated topology from Double Fence Gate

--- Workshop/Contents/mods/LMION_Core/42/media/lua/shared/LMION/Catalog/LargeGates/Metal/DoubleFenceGate.lua
+++ Workshop/Contents/mods/LMION_Core/42/media/lua/shared/LMION/Catalog/LargeGates/Metal/DoubleFenceGate.lua
@@ -4,24 +4,6 @@ return {
     entity = "Base.DoubleFenceGate",
     inherits = "LargeGates.Metal.Pipe",
 
-    topology = {
-        type = "largeGate",
-        leaves = {
-            A = { indices = { N = { 1, 2 }, W = { 4, 3 } } },
-            B = { indices = { N = { 3, 4 }, W = { 2, 1 } } },
-        },
-        layout = {
-            N = {
-                closed = { { 0, 0 }, { 1, 0 }, { 2, 0 }, { 3, 0 } },
-                open = { { 0, 0 }, { 0, 1 }, { 3, 1 }, { 3, 0 } },
-            },
-            W = {
-                closed = { { 0, 0 }, { 0, -1 }, { 0, -2 }, { 0, -3 } },
-                open = { { 0, 0 }, { 1, 0 }, { 1, -3 }, { 0, -3 } },
-            },
-        },
-    },
-
     geometry = {
         N = {
             A = {
