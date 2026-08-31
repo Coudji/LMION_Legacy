9b563dac94cd32c0ade13f01d7fcc81d9be39dee
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-31 18:57:24 +0200

    Remove duplicated topology from Wrought Iron Gate

--- Workshop/Contents/mods/LMION_Core/42/media/lua/shared/LMION/Catalog/LargeGates/Metal/LargeWroughtIronGate.lua
+++ Workshop/Contents/mods/LMION_Core/42/media/lua/shared/LMION/Catalog/LargeGates/Metal/LargeWroughtIronGate.lua
@@ -4,20 +4,26 @@ return {
     entity = "Base.LargeWroughtIronGate",
     inherits = "LargeGates.Metal.Forged",
 
-    topology = {
-        type = "largeGate",
-        leaves = {
-            A = { indices = { N = { 1, 2 }, W = { 4, 3 } } },
-            B = { indices = { N = { 3, 4 }, W = { 2, 1 } } },
-        },
-        layout = {
-            N = { closed = { {0,0},{1,0},{2,0},{3,0} }, open = { {0,0},{0,1},{3,1},{3,0} } },
-            W = { closed = { {0,0},{0,-1},{0,-2},{0,-3} }, open = { {0,0},{1,0},{1,-3},{0,-3} } },
-        },
-    },
-
     geometry = {
-        N = { A = { { closed = "fixtures_doors_fences_01_34", open = "fixtures_doors_fences_01_39" }, { closed = "fixtures_doors_fences_01_35", open = "fixtures_doors_fences_01_38" } }, B = { { closed = "fixtures_doors_fences_01_42", open = "fixtures_doors_fences_01_46" }, { closed = "fixtures_doors_fences_01_43", open = "fixtures_doors_fences_01_47" } } },
-        W = { A = { { closed = "fixtures_doors_fences_01_33", open = "fixtures_doors_fences_01_36" }, { closed = "fixtures_doors_fences_01_32", open = "fixtures_doors_fences_01_37" } }, B = { { closed = "fixtures_doors_fences_01_41", open = "fixtures_doors_fences_01_45" }, { closed = "fixtures_doors_fences_01_40", open = "fixtures_doors_fences_01_44" } } },
+        N = {
+            A = {
+                { closed = "fixtures_doors_fences_01_34", open = "fixtures_doors_fences_01_39" },
+                { closed = "fixtures_doors_fences_01_35", open = "fixtures_doors_fences_01_38" },
+            },
+            B = {
+                { closed = "fixtures_doors_fences_01_42", open = "fixtures_doors_fences_01_46" },
+                { closed = "fixtures_doors_fences_01_43", open = "fixtures_doors_fences_01_47" },
+            },
+        },
+        W = {
+            A = {
+                { closed = "fixtures_doors_fences_01_33", open = "fixtures_doors_fences_01_36" },
+                { closed = "fixtures_doors_fences_01_32", open = "fixtures_doors_fences_01_37" },
+            },
+            B = {
+                { closed = "fixtures_doors_fences_01_41", open = "fixtures_doors_fences_01_45" },
+                { closed = "fixtures_doors_fences_01_40", open = "fixtures_doors_fences_01_44" },
+            },
+        },
     },
 }
