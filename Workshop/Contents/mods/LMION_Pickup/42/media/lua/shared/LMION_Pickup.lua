6fabc48bbfcf24a6b0f6501f9abc326132beb04b
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-31 18:16:45 +0200

    Install Legacy parity after Large Gate placement

--- Workshop/Contents/mods/LMION_Pickup/42/media/lua/shared/LMION_Pickup.lua
+++ Workshop/Contents/mods/LMION_Pickup/42/media/lua/shared/LMION_Pickup.lua
@@ -2,8 +2,10 @@ local MoveableAdapter = require "LMION/Pickup/MoveableAdapter"
 local LargeGatePickup = require "LMION/Pickup/LargeGatePickup"
 local ParcelPresentation = require "LMION/Pickup/ParcelPresentation"
 local LargeGatePlacement = require "LMION/Pickup/LargeGatePlacement"
+local LargeGatePlacementParity = require "LMION/Pickup/LargeGatePlacementParity"
 
 MoveableAdapter.install()
 LargeGatePickup.install()
 ParcelPresentation.install()
 LargeGatePlacement.install()
+LargeGatePlacementParity.install()
