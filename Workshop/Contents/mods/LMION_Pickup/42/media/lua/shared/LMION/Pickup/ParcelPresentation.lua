4e1d2efb52ef6b1f96e92ebe23a848e6defc8b84
Coudji <106971118+Coudji@users.noreply.github.com>
2026-08-31 18:11:57 +0200

    Finalize Large Gate parcel presentation after vanilla transfer

---
+++ Workshop/Contents/mods/LMION_Pickup/42/media/lua/shared/LMION/Pickup/ParcelPresentation.lua
@@ -0,0 +1,75 @@
+local LargeGatePickup = require "LMION/Pickup/LargeGatePickup"
+
+local ParcelPresentation = {}
+
+local installed = false
+
+
+local function finalize(item)
+    local identity = LargeGatePickup.getParcelIdentity(item)
+    if identity == nil then
+        return item
+    end
+
+    local runtime = LargeGatePickup.getRuntime(identity.definitionId)
+    if runtime == nil then
+        return item
+    end
+
+    item:setActualWeight(runtime.weight)
+    item:setWeight(runtime.weight)
+    item:setName(
+        runtime.displayName
+            .. " "
+            .. tostring(identity.leaf)
+            .. " ("
+            .. tostring(identity.partIndex)
+            .. "/2)"
+    )
+    item:setCustomName(true)
+
+    if isClient() and sendItemStats ~= nil then
+        sendItemStats(item)
+    end
+
+    return item
+end
+
+
+function ParcelPresentation.install()
+    if installed then
+        return
+    end
+
+    require "Moveables/ISMoveableSpriteProps"
+
+    local previousInternal = ISMoveableSpriteProps.pickUpMoveableInternal
+    ISMoveableSpriteProps.pickUpMoveableInternal = function(
+        self,
+        character,
+        square,
+        object,
+        sprInstance,
+        spriteName,
+        createItem,
+        rotating
+    )
+        local item = previousInternal(
+            self,
+            character,
+            square,
+            object,
+            sprInstance,
+            spriteName,
+            createItem,
+            rotating
+        )
+
+        return finalize(item)
+    end
+
+    installed = true
+end
+
+
+return ParcelPresentation
