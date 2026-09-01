147b99e413f45b02b246b0a44cc2bf1f404a2d0e
Coudji <106971118+Coudji@users.noreply.github.com>
2026-09-01 11:45:19 +0200

    Mark Grey Garage Door topology

--- Workshop/Contents/mods/LMION_Core/42/media/lua/shared/LMION/Catalog/GarageDoors/GreyGarageDoor.lua
+++ Workshop/Contents/mods/LMION_Core/42/media/lua/shared/LMION/Catalog/GarageDoors/GreyGarageDoor.lua
@@ -4,6 +4,8 @@ return {
     entity = "Base.GreyGarageDoor",
     inherits = "GarageDoors.Solid",
 
+    topology = { type = "garage" },
+
     geometry = {
         N = {
             START = { closed = "walls_garage_01_51", open = "walls_garage_01_59" },
