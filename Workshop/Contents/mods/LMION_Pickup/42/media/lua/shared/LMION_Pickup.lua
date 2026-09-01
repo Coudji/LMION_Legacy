bcd30bd8b8fca7d7ce5f63755f02e1563079f351
Coudji <106971118+Coudji@users.noreply.github.com>
2026-09-01 12:11:23 +0200

    Install garage pickup

--- Workshop/Contents/mods/LMION_Pickup/42/media/lua/shared/LMION_Pickup.lua
+++ Workshop/Contents/mods/LMION_Pickup/42/media/lua/shared/LMION_Pickup.lua
@@ -1,11 +1,13 @@
 local MoveableAdapter = require "LMION/Pickup/MoveableAdapter"
 local LargeGatePickup = require "LMION/Pickup/LargeGatePickup"
+local GaragePickup = require "LMION/Pickup/GaragePickup"
 local ParcelPresentation = require "LMION/Pickup/ParcelPresentation"
 local LargeGatePlacement = require "LMION/Pickup/LargeGatePlacement"
 local LargeGatePlacementParity = require "LMION/Pickup/LargeGatePlacementParity"
 
 MoveableAdapter.install()
 LargeGatePickup.install()
+GaragePickup.install()
 ParcelPresentation.install()
 LargeGatePlacement.install()
 LargeGatePlacementParity.install()
