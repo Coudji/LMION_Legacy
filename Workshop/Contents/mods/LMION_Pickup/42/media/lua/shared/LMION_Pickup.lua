8bf3cb4f39efaba2feb875c105618cc29f02edd5
Coudji <106971118+Coudji@users.noreply.github.com>
2026-09-01 12:45:06 +0200

    Install garage toolbar adapter

--- Workshop/Contents/mods/LMION_Pickup/42/media/lua/shared/LMION_Pickup.lua
+++ Workshop/Contents/mods/LMION_Pickup/42/media/lua/shared/LMION_Pickup.lua
@@ -1,6 +1,7 @@
 local MoveableAdapter = require "LMION/Pickup/MoveableAdapter"
 local LargeGatePickup = require "LMION/Pickup/LargeGatePickup"
 local GaragePickup = require "LMION/Pickup/GaragePickup"
+local GarageToolbarAdapter = require "LMION/Pickup/GarageToolbarAdapter"
 local ParcelPresentation = require "LMION/Pickup/ParcelPresentation"
 local LargeGatePlacement = require "LMION/Pickup/LargeGatePlacement"
 local LargeGatePlacementParity = require "LMION/Pickup/LargeGatePlacementParity"
@@ -8,6 +9,7 @@ local LargeGatePlacementParity = require "LMION/Pickup/LargeGatePlacementParity"
 MoveableAdapter.install()
 LargeGatePickup.install()
 GaragePickup.install()
+GarageToolbarAdapter.install()
 ParcelPresentation.install()
 LargeGatePlacement.install()
 LargeGatePlacementParity.install()
